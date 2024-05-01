f236565b5d060c3a7d9a5119ae9587e7ed8f1201
Girafskind <bmo@girafskind.dk>
2024-05-01 13:48:08 +0200

    Added type hints and is now parsing the CSV data into a dictionary for easier running config command sets

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import logging
 import sys
 from netmiko import ConnectHandler
 from netmiko import NetmikoAuthenticationException, NetMikoTimeoutException
-from datetime import datetime
+from datetime import datetime, timedelta
 from getpass import getpass
 
 # Default command
@@ -32,31 +32,62 @@ def main():
     start_time = datetime.now()
 
     with open('devices.csv', 'r') as devices:
-        data = list(csv.reader(devices, delimiter=';'))
+        csv_data = list(csv.reader(devices, delimiter=';'))
+
+    dict_of_devices = convert_csvdata_to_dict(csv_data)
+
+    device_commands_list = []
+    for key, list_of_commands in dict_of_devices.items():
+        for command in list_of_commands:
+            device_commands_list.append([key,command])
 
     # Create queues of threads, divided into lists of 100
-    queues = queue_threads(data)
+    list_of_queues = queue_threads(device_commands_list)
     # Run each thread queue at the time
-    thread_time = run_threads(queues)
+    thread_time = run_threads(list_of_queues)
 
     print("\nOperation took: {}".format(datetime.now() - start_time))
     input("\nPress Enter to continue...")
 
 
-def queue_threads(devices_to_queue, threads=100):
+def convert_csvdata_to_dict(csv_data_tobe_converted: list) -> dict:
+    """
+    Convert the list of devices into a dictionary
+    :param csv_data_tobe_converted: List with list of devices
+    :return: Dictionary of devices
+    """
+    device_dict = {}
+
+    for entry in csv_data_tobe_converted:
+        device_ip = entry[0]
+        commands_for_device = []
+        if len(entry[1]) > 0:
+            commands_for_device.append(entry[1])
+        else:
+            commands_for_device.append(CONFIGSET)
+
+        if device_ip in device_dict.keys():
+            device_dict[device_ip] += commands_for_device
+        else:
+            device_dict[device_ip] = commands_for_device
+
+    return device_dict
+
+
+def queue_threads(devices_to_queue: list, max_concurrent_threads=100) -> list:
     """
     Queue threads in batches of specific number of threads, default is 100.
     :param devices_to_queue: The device to be added
-    :param threads: Number of threads to queue
+    :param max_concurrent_threads: Number of threads to queue
     :return: List of queues with threads
     """
     thread_queue = []
     thread_queue_list = []
     while len(devices_to_queue) > 0:
-        while len(thread_queue) <= threads:
+        while len(thread_queue) <= max_concurrent_threads:
             if len(devices_to_queue) > 0:
-                command = devices_to_queue.pop(0)
-                thread_queue.append(threading.Thread(target=runcommand, args=(command,)))
+                device_from_queue = devices_to_queue.pop(0)
+                thread_queue.append(threading.Thread(target=runcommand, args=(device_from_queue,)))
             else:
                 break
         thread_queue_list.append(thread_queue)
@@ -65,10 +96,10 @@ def queue_threads(devices_to_queue, threads=100):
     return thread_queue_list
 
 
-def run_threads(thread_queue):
+def run_threads(thread_queue: list) -> timedelta:
     """
     Start the given thread queue
-    :param thread_queue: The thread queue to start
+    :param thread_queue: The thread queue list to start
     :return: Time the whole operation took
     """
     start_time = datetime.now()
@@ -82,20 +113,21 @@ def run_threads(thread_queue):
     return duration
 
 
-def runcommand(devicestring):
+def runcommand(device_to_configure: str):
     """
     Function for pushing out command to device
-    :param devicestring: String containing the IP of the device
+    :param device_to_configure: String containing the IP of the device
     :return: Nothing
     """
-    device_handler = {'username': my_user, 'password': my_pass, 'ip': devicestring[0],
+    device_handler = {'username': my_user, 'password': my_pass, 'ip': device_to_configure[0],
                       'device_type': "cisco_ios"}
+    command_to_send = device_to_configure[1]
     try:
         switch_conn = ConnectHandler(**device_handler)
         logger.debug('Handler connected to ' + device_handler['ip'])
         print("Handler connected")
         hostname = switch_conn.find_prompt().replace("#", "")
-        cmd_result = switch_conn.send_command(CONFIGSET)
+        cmd_result = switch_conn.send_command(command_to_send)
 
         output = open(device_handler['ip'] + "_" + hostname + ".txt", "w")
         output.write("#" * len(" Hostname : " + hostname + " ")+"\n")
